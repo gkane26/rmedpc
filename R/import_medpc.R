9b8db7a64a89f0f43c8ed2731233a5cde6edbb20
gkane <gkane@rowland.harvard.edu>
2019-01-27 08:16:09 -0500

    initial commit

---
+++ R/import_medpc.R
@@ -0,0 +1,64 @@
+#' import data from MedPC file to an R list
+#'
+#' @param filename full path to MedPC data file
+#' @return list containing each variable in MedPC data file, referenced by variable name
+#' @export
+#' @examples
+#' example_file = system.file("extdata", "example_data", package = "rmedpc")
+#' import_medpc(example_file)
+import_medpc <- function(filename){
+
+  if(missing(filename)){
+    library(svDialogs)
+    filename = dlgOpen()$res
+  }
+
+  #initialize list of variables
+  varList = list()
+
+  #Read file line by line, split line by labels
+  line = scan(file=filename, character(0), sep="\n", strip.white=TRUE)
+  line = gsub("\\s+", " ", line)
+  spl = strsplit(line, ": ")
+
+  #Pull out general info
+  for(i in 1:10) varList[[spl[[i]][1]]] = spl[[i]][2]
+
+  #remove general info, split rest into label & values
+  line=line[-(1:10)]
+  spl = strsplit(line, " ")
+
+  ############################################################
+  #Identify variables and arrays, assign values
+  #Loop through every line
+  #if character followed by one number, assign variable letter to number
+  #if character alone, assign array to all following variables until next character
+  ############################################################
+
+  getLabels = function(x) strsplit(x[[1]][1], ":")[[1]][1] #takes list, returns label at beginning of line
+  labels = sapply(spl, getLabels)
+
+  cnt=1
+  while(cnt<=length(spl)){
+
+    if(is.na(strtoi(labels[cnt]))){ #if label is a letter (start of variable or array)
+
+      if(length(spl[[cnt]]) == 2){ #if variable
+        varList[[labels[cnt]]] = as.numeric(spl[[cnt]][2])
+      }else if(length(spl[[cnt]]) == 1){ #if start of array
+        current = labels[[cnt]]
+        varList[[current]] = NULL
+        totalRay = NULL
+      }
+
+    }else{ #if label is number (continuation of array)
+      totalRay = c(totalRay, as.numeric(spl[[cnt]][2:length(spl[[cnt]])]))
+      varList[[current]] = totalRay
+    }
+
+    cnt = cnt +1
+  }
+
+  return(varList)
+
+}
